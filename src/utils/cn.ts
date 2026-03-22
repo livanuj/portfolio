e54bfb6f0b1ad301389d48087dd132fa72b26db7
Anuj Shrestha <anuj@taktikal.is>
2026-03-22 00:38:24 +0000

    add utils

---
+++ src/utils/cn.ts
@@ -0,0 +1,11 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Combines multiple class names and merges Tailwind CSS classes intelligently
+ * @param inputs - Class names to combine
+ * @returns Merged class string
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
